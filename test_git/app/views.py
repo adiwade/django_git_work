970eab81864a633b2d8668be6be0078ac9de1455
adiwade <meettoadiwade@gmail.com>
2021-03-21 12:11:40 +0530

    one view is added

--- test_git/app/views.py
+++ test_git/app/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+def show_add(request):
+    return HttpResponse("from add view.....")
 def show_message(request):
     return HttpResponse("from view.....")
